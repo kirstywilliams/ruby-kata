2acd3fd18d24ff1a04d7939cf98e451422aa88ed
Kirsty Williams <kirsty@digitisedlipstick.com>
2015-02-22 15:48:31 +0000

    Digitise (CodeWars)

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Some kata completed for [CodeWars](http://www.codewars.com).
 2. vampire_test(a, b)
 
 	Create a function that can receive two 'fangs' and determine if the product of the two is a valid vampire number.
+3. digitise(n)
+
+	Given a non-negative integer, return an array / a list of the individual digits in order.
--- ruby_kata.rb
+++ ruby_kata.rb
@@ -3,7 +3,7 @@ class RubyKata
 	# This function should return n!
 	def factorial(n)
 
-		# return nil if non +ve
+		# return nil if -ve
 		return nil if n < 0
 		# return 1 if 0 or recursively call factorial(n-1)
 		n == 0 ? 1 : n * factorial(n - 1)
@@ -17,4 +17,15 @@ class RubyKata
 		"#{a}#{b}".chars.sort == (a*b).to_s.chars.sort
 		
 	end
+
+	# This function should return an array of the individual 
+	# digits of n in order.
+	def digitize(n)
+		
+		# return nil if -ve
+		return nil if n < 0
+		n.to_s.chars.map(&:to_i)
+	
+	end
+	
 end
